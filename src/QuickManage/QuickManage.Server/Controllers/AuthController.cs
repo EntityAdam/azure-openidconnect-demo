023c403bf0373bc07a35d8b3634d8bb93482522f
Adam Vincent <entityadam@outlook.com>
2019-03-15 17:31:55 -0400

    Added project files and Readme

---
+++ src/QuickManage/QuickManage.Server/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickManage.Server.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("signin")]
+        public IActionResult SignIn(string returnUrl = null)
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = returnUrl ?? "/" });
+        }
+
+        [HttpPost]
+        [Route("signout")]
+        public async Task SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
---
+++ src/QuickManage/QuickManage.Server/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+
+namespace QuickManage.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorComponents<App.Startup>();
+
+            services.AddMvc();
+
+            services.AddAuthentication(options =>
+                    {
+                        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    })
+                    .AddOpenIdConnect(options => Configuration.Bind("AzureOpenIdConnect", options))
+                    .AddCookie();
+
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
+            services.AddHttpClient();
+            services.AddScoped<HttpClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+            app.UseRazorComponents<App.Startup>();
+        }
+    }
+}
